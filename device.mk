a77887fda184ed27c7c735ab275cae8da885f9ff
Droneship <81514881+Seifood@users.noreply.github.com>
2022-10-24 16:04:22 +0200

    Fix build error

--- device.mk
+++ device.mk
@@ -17,7 +17,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
-    android.hardware.boot@1.1-impl-qti.recovery
 
 PRODUCT_PACKAGES += \
     bootctrl.holi
